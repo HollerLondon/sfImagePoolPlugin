beafc8e3dd5acdf64eaa45534cad74e2d1632f20
angelsk <jocarter+github@holler.co.uk>
2011-06-23 10:45:52 +0000

    Adding tag option to filter as well - to prevent errors in admin
    git-svn-id: https://trac.clyde.hollerdigital.co.uk/svn/symfonyplugins/sfImagePoolPlugin/branches/release@351 b16ae86e-47c9-4a5c-af2a-31f9ae5f50b3

--- lib/filter/doctrine/PluginsfImagePoolImageFormFilter.class.php
+++ lib/filter/doctrine/PluginsfImagePoolImageFormFilter.class.php
@@ -14,24 +14,29 @@ abstract class PluginsfImagePoolImageFormFilter extends BasesfImagePoolImageForm
   {
     parent::setup();
     
-    $query = Doctrine_Core::getTable('Tag')
-               ->createQuery('t')
-               ->where(
-                 'id IN(SELECT tag_id FROM tagging WHERE taggable_model = "sfImagePoolImage")'
-               );
-    
-    $this->widgetSchema['tag'] = new sfWidgetFormDoctrineChoice(array(
-      'model'         => 'tag',
-      'multiple'      => true,
-      'expanded'      => true,
-      'query'         => $query,
-    ));
-    
-    $this->validatorSchema['tag'] = new sfValidatorDoctrineChoice(array(
-      'model'         => 'tag',
-      'multiple'      => true,
-      'required'      => false
-    ));
+    $image = new sfImagePoolImage();
+    if ($image->option('tagging'))
+    {
+      $query = Doctrine_Core::getTable('Tag')
+                 ->createQuery('t')
+                 ->where(
+                   'id IN(SELECT tag_id FROM tagging WHERE taggable_model = "sfImagePoolImage")'
+                 );
+      
+      $this->widgetSchema['tag'] = new sfWidgetFormDoctrineChoice(array(
+        'model'         => 'tag',
+        'multiple'      => true,
+        'expanded'      => true,
+        'query'         => $query,
+      ));
+      
+      $this->validatorSchema['tag'] = new sfValidatorDoctrineChoice(array(
+        'model'         => 'tag',
+        'multiple'      => true,
+        'required'      => false
+      ));
+    }
+    $image->free(true);
   }
   
   public function addTagColumnQuery(Doctrine_Query $q, $field, $value)
