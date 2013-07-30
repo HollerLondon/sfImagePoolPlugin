8c10135255620c056a2e0d0272c40f998f08be3e
Jo Carter <github@jocarter.co.uk>
2013-07-30 06:26:27 -0700

    Styling upload crop page as per other admin pages

--- modules/sfImagePoolAdmin/templates/uploadCropSuccess.php
+++ modules/sfImagePoolAdmin/templates/uploadCropSuccess.php
@@ -1,19 +1,40 @@
 <?php use_helper('ImagePool') ?>
 
-<h2>Upload Custom Crop</h2>
-<p>Your upload will override any automatic crop that would be performed to the same dimensions (as your image).</p>
+<div id="sf_admin_container">
+  <h1>Image Pool</h1>
 
-<div class="sf_admin_form">
-<?php echo $form->renderFormTag(url_for('sf_image_pool_upload_crop', $image)) ?>
-    <fieldset>
-        <p><?php echo pool_image_tag($image, '200', 'scale', array('class' => 'image-preview')) ?></p>
-        <table>
-          <?php echo $form ?>
-        </table>
-        <div>
-            <input type="submit" value="Upload" />
-            <br />
-            <?php echo link_to('Cancel', 'sfImagePoolAdmin/index') ?>
-        </div>
-    </fieldset>
-</form>
+  <div id="sf_admin_content">
+  
+    <h2>Upload Custom Crop</h2>
+    <p>Your upload will override any automatic crop that would be performed to the same dimensions (as your image).</p>
+    
+    <div class="sf_admin_form">
+      <?php echo $form->renderFormTag(url_for('sf_image_pool_upload_crop', $image)) ?>
+        <fieldset>
+          <p><?php echo pool_image_tag($image, '200', 'scale', array('class' => 'image-preview')) ?></p>
+          
+          <div class="sf_admin_form_row sf_admin_text sf_admin_form_field_cropped_image<?php if ($form['cropped_image']->hasError()) echo ' errors'; ?>">
+            <?php echo $form['cropped_image']->renderError(); ?>
+          
+            <div>
+              <label for="modules_youtube_id"><?php echo $form['cropped_image']->renderLabel(); ?></label>
+              <div class="content">
+                <?php echo $form['cropped_image']->render(); ?>
+              </div>
+          	</div>
+          </div>
+          
+          <?php echo $form->renderHiddenFields(); ?>
+        </fieldset>
+        
+        <ul class="sf_admin_actions">
+          <li class="sf_admin_action_delete"><?php echo link_to('Cancel', 'sfImagePoolAdmin/index') ?></li>  
+          <li class="sf_admin_action_save"><input type="submit" value="Upload"></li>
+        </ul>
+      </form>
+    
+    </div>
+  
+  </div>
+
+</div>
